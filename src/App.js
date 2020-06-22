ee962be7e8a0fe13227a6483de3335a8f7c54d4a
Mubtasim Mahmud <mubtasimmahmud20@gmail.com>
2020-06-22 18:55:54 +1000

    Updated preview to update with slider

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
-import React, { useState } from "react";
-import s from "./app.module.scss";
-import GetURL from "./components/GetURL/GetURL";
-import Video from "./components/Video/Video";
+import React, { useState } from 'react';
+import s from './app.module.scss';
+import Slider from '@material-ui/core/Slider';
+import GetURL from './components/GetURL/GetURL';
+import Video from './components/Video/Video';
 
 function App() {
-    const [videoUrl, setVideoUrl] = useState(null);
+  const [videoUrl, setVideoUrl] = useState(null);
+  const [time, setTime] = useState([0, 5]);
+  const [max, setMax] = useState(0);
 
-    const getVideoId = () => {
-        const parts = videoUrl.split("/");
-        return parts[parts.length - 1].split("?")[0];
-    };
+  const getVideoId = () => {
+    const parts = videoUrl.split('/');
+    return parts[parts.length - 1].split('?')[0];
+  };
 
-    return (
-        <div className={s.container}>
-            <h1 className={s.header}>Glipher</h1>
-            <GetURL setVideoUrl={setVideoUrl} />
-            {videoUrl && (
-                <Video
-                    setVideoUrl={setVideoUrl}
-                    url={videoUrl}
-                    videoId={getVideoId()}
-                />
-            )}
-        </div>
-    );
+  const handleTimeChange = (event, newValue) => {
+    setTime(newValue);
+  };
+
+  return (
+    <div className={s.container}>
+      <h1 className={s.header}>Glipher</h1>
+      <GetURL setVideoUrl={setVideoUrl} />
+      {videoUrl && (
+        <>
+          <div className={s.section}>
+            <Slider
+              max={max}
+              value={time}
+              onChange={handleTimeChange}
+              valueLabelDisplay="on"
+              aria-labelledby="range-slider"
+            />
+          </div>
+          <Video
+            start={time[0]}
+            end={time[1]}
+            setMax={setMax}
+            setVideoUrl={setVideoUrl}
+            url={videoUrl}
+            videoId={getVideoId()}
+          />
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,57 +1,58 @@
-import React, { useEffect } from "react";
-import s from "./video.module.scss";
+import React, { useEffect } from 'react';
+import s from './video.module.scss';
 
-const Video = ({ url, videoId }) => {
-    useEffect(() => {
-        if (!window["YT"]) {
-            const tag = document.createElement("script");
-            tag.src = "https://www.youtube.com/iframe_api";
+const Video = ({ url, start, end, videoId, setMax }) => {
+  useEffect(() => {
+    if (!window['YT']) {
+      const tag = document.createElement('script');
+      tag.src = 'https://www.youtube.com/iframe_api';
 
-            window["onYouTubeIframeAPIReady"] = loadVideo;
+      window['onYouTubeIframeAPIReady'] = loadVideo;
 
-            const firstScriptTag = document.getElementsByTagName("script")[0];
-            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-        } else {
-            loadVideo();
-        }
-    }, [url]);
+      const firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    } else {
+      loadVideo();
+    }
+  }, [url, start, end]);
 
-    const loadVideo = () => {
-        new window["YT"].Player(`player-${videoId}`, {
-            videoId,
-            events: {
-                onReady: onPlayerReady,
-                onStateChange: onPlayerStateChange,
-            },
-        });
-    };
+  const loadVideo = () => {
+    new window['YT'].Player(`player-${videoId}`, {
+      videoId,
+      events: {
+        onReady: onPlayerReady,
+        onStateChange: onPlayerStateChange,
+      },
+    });
+  };
 
-    const onPlayerReady = (event) => {
-        loopStart(event);
-        event.target.mute();
-        event.target.playVideo();
-    };
+  const onPlayerReady = (event) => {
+    setMax(event.target.getDuration());
+    loopStart(event);
+    event.target.mute();
+    event.target.playVideo();
+  };
 
-    const loopStart = (event) => {
-        event.target.seekTo(7);
-    };
+  const loopStart = (event) => {
+    event.target.seekTo(start);
+  };
 
-    const onPlayerStateChange = (event) => {
-        if (event.data === window["YT"].PlayerState.PLAYING) {
-            setTimeout(loopStart, 5000, event); // After 5 seconds, restart the loop
-        }
-    };
+  const onPlayerStateChange = (event) => {
+    if (event.data === window['YT'].PlayerState.PLAYING) {
+      setTimeout(loopStart, (end - start) * 1000, event); // After 5 seconds, restart the loop
+    }
+  };
 
-    return (
-        <div key={url} id="playerContainer" className={s.container}>
-            <div
-                src={url + "?enablejsapi=1&controls=0"}
-                id={`player-${videoId}`}
-                type="text/html"
-                frameborder="0"
-            ></div>
-        </div>
-    );
+  return (
+    <div key={url + start + end} id="playerContainer" className={s.container}>
+      <div
+        src={url + '?enablejsapi=1&controls=0'}
+        id={`player-${videoId}`}
+        type="text/html"
+        frameborder="0"
+      ></div>
+    </div>
+  );
 };
 
 export default Video;
