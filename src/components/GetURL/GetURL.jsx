5d4e551d3cd1c53dec40df3de082bd979b9e4883
Mubtasim Mahmud <mubtasim.mahmud@monash.edu>
2020-06-23 12:27:33 +1000

    Enter key gets video

--- src/components/GetURL/GetURL.jsx
+++ src/components/GetURL/GetURL.jsx
@@ -1,28 +1,33 @@
-import React, { useState } from 'react';
-import s from './geturl.module.scss';
+import React, { useState } from "react";
+import s from "./geturl.module.scss";
 
 const GetURL = ({ setVideoUrl }) => {
-  const [url, setUrl] = useState('');
+    const [url, setUrl] = useState("");
 
-  return (
-    <div className={s.inputGroup}>
-      <input
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}
-        className={s.input}
-        type="text"
-        placeholder="Enter YouTube video URL"
-      />
-      <button
-        onClick={() => {
-          setVideoUrl(url.replace('watch?v=', 'embed/'));
-        }}
-        className={s.button}
-      >
-        Get
-      </button>
-    </div>
-  );
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") setVideoUrl(url.replace("watch?v=", "embed/"));
+    };
+
+    return (
+        <div className={s.inputGroup}>
+            <input
+                value={url}
+                onChange={(e) => setUrl(e.target.value)}
+                onKeyDown={handleKeyDown}
+                className={s.input}
+                type="text"
+                placeholder="Enter YouTube video URL"
+            />
+            <button
+                onClick={() => {
+                    setVideoUrl(url.replace("watch?v=", "embed/"));
+                }}
+                className={s.button}
+            >
+                Get
+            </button>
+        </div>
+    );
 };
 
 export default GetURL;
